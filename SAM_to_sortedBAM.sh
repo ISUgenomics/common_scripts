c5f157bca12d3287ebc2b6a3776ddd58ac67e643
Arun Seetharam <arnstrm@iastate.edu>
2019-09-12 08:57:29 -0500

    fastq splitter added

--- SAM_to_sortedBAM.sh
+++ SAM_to_sortedBAM.sh
@@ -4,5 +4,5 @@
 module load samtools
 
 SAM="$1"
-samtools view --threads 16 -b -o ${SAM%.*}.bam ${SAM}
-samtools sort -o ${SAM%.*}_sorted.bam -T ${SAM%.*}_temp --threads 16 ${SAM%.*}.bam
+samtools view --threads 36 -b -o ${SAM%.*}.bam ${SAM}
+samtools sort -o ${SAM%.*}_sorted.bam -T ${SAM%.*}_temp --threads 36 ${SAM%.*}.bam
