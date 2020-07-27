a45c72b1c5875c800fc261f530ab40ec70126779
Andrew Severin <severin@iastate.edu>
2020-07-27 11:53:17 -0500

    forgot to remove the @ symbol in fastq2fasta.sh

--- fastq2fasta.sh
+++ fastq2fasta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat $1 | perl -pe 's/ /_/g' | paste - - - - | awk '{print ">"$1;print $2}' > ${1%%.*}.fasta
+cat $1 | perl -pe 's/ /_/g' | paste - - - - | awk '{print ">"$1;print $2}' | perl -pe 's/^>@/>/g' > ${1%%.*}.fasta
