ebf372c73cfe21a5d3731a47ec6a822905b76a87
Andrew Severin <severin@iastate.edu>
2021-04-26 10:54:35 -0500

    updated how to run it, ie which file

--- nanoVersions.sh
+++ nanoVersions.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# nanoVerions.sh report_FAO68032_20210219_1551_cc50bf20.md 
+# you will need to find the markdown FAO report file.
+
 guppy=$(cat $1 | grep guppy | awk '{print $NF}') # guppy version
 minknow=$(cat $1 | grep distribution_version| awk '{print $NF}') # MinKNOW version
 bream=$(cat $1 | grep protocols_version| awk '{print $NF}') # Bream version
